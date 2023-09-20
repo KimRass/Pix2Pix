ac2ae62a9bce569c1ab562a96236e4e39095b521
KimRass <rmx1000@yonsei.ac.kr>
2023-09-20 19:02:29 +0900

    Working on FID

--- evaluate.py
+++ evaluate.py
@@ -9,18 +9,19 @@ from numpy import iscomplexobj
 from numpy.random import random
 from scipy.linalg import sqrtm
 
-# calculate frechet inception distance
-def calculate_fid(act1, act2):
-	# calculate mean and covariance statistics
-	mu1, sigma1 = act1.mean(axis=0), cov(act1, rowvar=False)
-	mu2, sigma2 = act2.mean(axis=0), cov(act2, rowvar=False)
-	# calculate sum squared difference between means
-	ssdiff = numpy.sum((mu1 - mu2)**2.0)
+
+def calculate_fid(feat1, feat2):
+	feat1 = torch.randn(16, 1024)
+	feat2 = torch.randn(16, 1024)
+	mu1, sigma1 = feat1.mean(axis=0), torch.cov(feat1)
+	mu2, sigma2 = feat2.mean(axis=0), torch.cov(feat2)
+	sum_squared_diff = torch.sum((mu1 - mu2) ** 2)
 	# calculate sqrt of product between cov
+    (sigma1 * sigma2) ** 0.5
 	covmean = sqrtm(sigma1.dot(sigma2))
 	# check and correct imaginary numbers from sqrt
 	if iscomplexobj(covmean):
 		covmean = covmean.real
 	# calculate score
-	fid = ssdiff + trace(sigma1 + sigma2 - 2.0 * covmean)
+	fid = sum_squared_diff + trace(sigma1 + sigma2 - 2.0 * covmean)
 	return fid
